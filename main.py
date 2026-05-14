67be7556af1848f76ad61db99eada4db4b0239f1
Arda <ardansks1457@gmail.com>
2026-05-14 22:57:45 +0300

    add json config for railway

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
-import os
+import json
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+# =========================
+# CONFIG OKUMA (JSON)
+# =========================
+with open("config.json", "r") as f:
+    config = json.load(f)
 
+TOKEN = config["TELEGRAM_TOKEN"]
+
+# =========================
+# BOT LOGIC
+# =========================
 async def cevap(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.text.lower() == "selam":
         await update.message.reply_text("selam")
 
+# =========================
+# BOT START
+# =========================
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, cevap))
 
-app.run_polling()	
+print("Bot çalışıyor...")
+app.run_polling()
