6e5d088661a116d5fd71206fda7d81ff7c0eab24
ardansks1457-ops <ardansks1457@gmail.com>
2026-05-14 23:52:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-if not TOKEN:
-    print("TELEGRAM_TOKEN bulunamadı!")
-    exit()
-
 async def cevap(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message and update.message.text:
-        if update.message.text.lower() == "selam":
-            await update.message.reply_text("selam")
+    text = update.message.text.lower()
+
+    if text == "selam":
+        await update.message.reply_text("selam")
 
 app = Application.builder().token(TOKEN).build()
 
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, cevap))
+app.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, cevap)
+)
 
-print("Bot çalışıyor...")
+print("Bot aktif!")
+
 app.run_polling()
